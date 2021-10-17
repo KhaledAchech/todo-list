a44ee216fb938dc193390f2db09b6f93ef128d3b
KhaledAchech <achechkhaled@gmail.com>
2021-10-17 18:57:45 +0100

    Material Ui and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FC} from 'react';
 import './App.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
-function App() {
+
+const App: FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       
-      </header>
+     <div className="header">
+       <div className="inputContainer">
+       <Box
+       sx={{ p: 2, border: '1px dashed grey', backgroundColor : '#A6B6DB'}}>
+       <Stack direction="row" spacing={3}>
+       <Stack spacing={2}>
+        <TextField id="task" label="Task..." variant="filled" />
+        <TextField type="number" id="deadline" label="Deadline (in Days) ..." variant="filled" />
+        </Stack>
+        <Button variant="contained" color="success">
+          Add Task
+        </Button>
+       </Stack>
+       </Box>
+       </div>
+       </div>
+     <div className="todoList"></div>
     </div>
   );
 }
