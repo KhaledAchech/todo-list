7b24d49b90ac404a17d6e035c87a1bb4a303e4b4
KhaledAchech <achechkhaled@gmail.com>
2021-10-17 19:08:59 +0100

    theme update

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,18 @@ const App: FC = () => {
   return (
     <div className="App">
      <div className="header">
-       <div className="inputContainer">
        <Box
-       sx={{ p: 2, border: '1px dashed grey', backgroundColor : '#A6B6DB'}}>
+       sx={{ p: 10, display :'center'}}>
        <Stack direction="row" spacing={3}>
        <Stack spacing={2}>
         <TextField id="task" label="Task..." variant="filled" />
         <TextField type="number" id="deadline" label="Deadline (in Days) ..." variant="filled" />
         </Stack>
-        <Button variant="contained" color="success">
+        <Button variant="contained" color="secondary">
           Add Task
         </Button>
        </Stack>
        </Box>
-       </div>
        </div>
      <div className="todoList"></div>
     </div>
